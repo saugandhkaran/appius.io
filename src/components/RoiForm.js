7412b7290cadc7718898aff843c141695b5254c4
Saugandh Karan <saugandhkaran@gmail.com>
2021-03-26 14:48:43 +0100

    Label change

--- src/components/RoiForm.js
+++ src/components/RoiForm.js
@@ -108,7 +108,7 @@ function RoiForm(props) {
       </div>
       </form>
       <br /><br />
-      <p>Please help us helping the customer win by sending your feedback: <a href = "mailto: titlesboreme@gmail.com">Send Feedback</a></p>
+      <p>Help the customer win by sending your feedback: <a href = "mailto: titlesboreme@gmail.com">Send Feedback</a></p>
   </div>
   )
 }
