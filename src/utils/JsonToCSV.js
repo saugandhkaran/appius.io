518932c2792782f71914b9d8e5b8bb467b68bb5e
Saugandh Karan <saugandhkaran@gmail.com>
2021-04-01 15:09:43 +0200

    Added dependency attribute in JsontoCSV

--- src/utils/JsonToCSV.js
+++ src/utils/JsonToCSV.js
@@ -1,36 +1,13 @@
-import exportFromJSON from 'export-from-json'
+import exportFromJSON from 'export-from-json';
+import FormEntries from '../resources/data/FormEntries';
 
 function JsonTOCSV(dataArray) {
-  const formula = {
-    impactedUsers: {
-      '6': '100%',
-      '5': '80% - 99%',
-      '4': '50% - 79%',
-      '3': '20% - 49%',
-      '2': '10 - 19%',
-      '1': 'Less than 10%'
-    },
-    frequency: {
-      '6': 'Daily',
-      '5': 'Few times a week',
-      '4': 'Weekly',
-      '3': 'Monthly',
-      '2': 'Few times a year',
-      '1': 'Once a year or less'
-    },
-    improvement: {
-      '6': '100%',
-      '5': '80% - 99%',
-      '4': '50% - 79%',
-      '3': '20% - 49%',
-      '2': '10 - 19%',
-      '1': 'Less than 10%'
-    }
-  }
+  const formula = new FormEntries();
   const data = dataArray.map(item => {
       item.impactedUsers = formula.impactedUsers[item.impactedUsers];
       item.frequency = formula.frequency[item.frequency];
       item.improvement = formula.improvement[item.improvement];
+      item.dependency = formula.dependency[item.dependency];
       return item;
   });
   const date = new Date();
